dcb0d774affaab242397fe1cea87919c25ee054c
Vasi Mihalca <vasi.mihalca@ww.com>
2024-02-13 21:25:05 +0200

    Thread adjustments 1

--- src/teachinlathe/widgets/quick_cycles/quick_cycles.py
+++ src/teachinlathe/widgets/quick_cycles/quick_cycles.py
@@ -3,20 +3,26 @@ from enum import Enum
 from PyQt5 import QtCore
 from qtpy import uic
 from qtpy.QtWidgets import QWidget
+from qtpyvcp.actions.machine_actions import issue_mdi
+from qtpyvcp.plugins import getPlugin
 from qtpyvcp.utilities import logger
 import tempfile
+
+from qtpyvcp.widgets.display_widgets.dro_widget import Axis
 
 from teachinlathe.widgets.smart_numpad_dialog import SmartNumPadDialog
 
 LOG = logger.getLogger(__name__)
 
 UI_FILE = os.path.join(os.path.dirname(__file__), "quick_cycles.ui")
+STATUS = getPlugin('status')
 
 
 class Page(Enum):
-    def __init__(self, index, title):
+    def __init__(self, index, title, next_btn_text="Load"):
         self.index = index
         self.title = title
+        self.next_btn_text = next_btn_text
 
     ROOT = (0, "Quick Cycles")
     TURNING = (1, "Turning")
@@ -25,8 +31,12 @@ class Page(Enum):
     CHAMFER = (4, "Chamfer")
     RADIUS = (5, "Radius")
     DRILLING = (6, "Drilling")
-    THREADING = (7, "Threading")
-    KEY_SLOT = (8, "Key Slot")
+    KEY_SLOT = (7, "Key Slot")
+    THREADING_1 = (8, "Threading (step 1)", "Next")
+    THREADING_2 = (9, "Threading (step 2)", "Next")
+    THREADING_3 = (10, "Threading (step 3)", "Load")
+    THREADING_4 = (10, "Threading (step 4)", None)
+    THREADING_5 = (10, "Threading (step 5)", "Load")
 
 
 class QuickCycles(QWidget):
@@ -35,6 +45,18 @@ class QuickCycles(QWidget):
     def __init__(self, parent=None):
         super(QuickCycles, self).__init__(parent)
         uic.loadUi(UI_FILE, self)
+
+        self.currentXValue = 0
+        self.currentZValue = 0
+
+        self.last_thread_x_start = None
+        self.last_thread_z_start = None
+        self.last_thread_x_end = None
+
+        self.pos = getPlugin('position')
+        getattr(self.pos, 'rel').notify(self.updateValues)
+        STATUS.task_mode.signal.connect(self.onTaskModeChanged)
+
         self.isExternalThread = True
         self.switchPage(Page.ROOT)
         self.btnTurning.clicked.connect(lambda: self.switchPage(Page.TURNING))
@@ -42,10 +64,12 @@ class QuickCycles(QWidget):
         self.btnFacing.clicked.connect(lambda: self.switchPage(Page.FACING))
         self.btnChamfer.clicked.connect(lambda: self.switchPage(Page.CHAMFER))
         self.btnRadius.clicked.connect(lambda: self.switchPage(Page.RADIUS))
-        self.btnThreading.clicked.connect(lambda: self.switchPage(Page.THREADING))
+        self.btnThreading.clicked.connect(lambda: self.switchPage(Page.THREADING_1))
         self.btnDrilling.clicked.connect(lambda: self.switchPage(Page.DRILLING))
         self.btnKeySlot.clicked.connect(lambda: self.switchPage(Page.KEY_SLOT))
-        self.btnBack.clicked.connect(lambda: self.switchPage(Page.ROOT))
+        self.btnBack.clicked.connect(self.btnOnBackClicked)
+        self.btnThreadingFinished.clicked.connect(self.onThreadingFinished)
+        self.btnThreadingGoDeeper.clicked.connect(self.onThreadingGoDeeper)
 
         self.turningXEnd.mousePressEvent = lambda _: self.openNumPad(self.turningXEnd)
         self.turningZEnd.mousePressEvent = lambda _: self.openNumPad(self.turningZEnd)
@@ -84,6 +108,7 @@ class QuickCycles(QWidget):
         self.threadingFirstPass.mousePressEvent = lambda _: self.openNumPad(self.threadingFirstPass)
         self.threadingXStart.mousePressEvent = lambda _: self.openNumPad(self.threadingXStart)
         self.threadingXEnd.mousePressEvent = lambda _: self.openNumPad(self.threadingXEnd)
+        self.newXEnd.mousePressEvent = lambda _: self.openNumPad(self.newXEnd)
         self.threadingZEnd.mousePressEvent = lambda _: self.openNumPad(self.threadingZEnd)
         self.threadingCompute.clicked.connect(self.computeThreadDepth)
         self.threadingCompAngle.mousePressEvent = lambda _: self.openNumPad(self.threadingCompAngle)
@@ -93,13 +118,65 @@ class QuickCycles(QWidget):
         self.keyslotDoc.mousePressEvent = lambda _: self.openNumPad(self.keyslotDoc)
         self.keyslotFeed.mousePressEvent = lambda _: self.openNumPad(self.keyslotFeed)
 
-        self.btnLoad.clicked.connect(self.onBtnLoadClicked)
+        self.btnNext.clicked.connect(self.onBtnNextClicked)
+        self.btnMoveToPosX.clicked.connect(self.moveToPosX)
+        self.btnMoveToPosZ.clicked.connect(self.moveToPosZ)
+
+    def updateValues(self, pos=None):
+        if self.stackedWidget.currentIndex() is not Page.THREADING_3.index:
+            return
+        if pos is None:
+            pos = getattr(self.pos, 'rel').getValue()
+
+        self.currentXValue = pos[Axis.X]
+        self.currentZValue = pos[Axis.Z]
+
+        self.last_thread_x_start = self.currentXValue
+        self.last_thread_z_start = self.currentZValue
+
+        if self.isExternalThread:
+            self.labelRetract.setText(str(round(self.currentXValue - float(self.labelMajorDiameter.text()), 1)))
+        else:
+            self.labelRetract.setText(str(round(float(self.labelMinorDiameter.text() - self.currentXValue), 1)))
+
+        if self.labelZEnd.text() is not None:
+            self.labelThreadLength.setText(str(round(abs(float(self.labelZEnd.text()) - self.currentZValue), 1)))
+
+    def onTaskModeChanged(self, mode):
+        if (self.stackedWidget.currentIndex() in [Page.THREADING_3.index, Page.THREADING_5.index]) and mode == 1:
+            print("----Threading finished----")
+            self.switchPage(Page.THREADING_4)
 
     def switchPage(self, page: Page):
         self.stackedWidget.setCurrentIndex(page.index)
         self.title.setText(page.title)
         self.btnBack.setVisible(page != Page.ROOT)
-        self.btnLoad.setVisible(page != Page.ROOT)
+        self.btnNext.setVisible(page != Page.ROOT and page.next_btn_text is not None)
+        if page.next_btn_text is not None:
+            self.btnNext.setText(page.next_btn_text)
+
+        if page == Page.THREADING_3:
+            pitch = self.threadingPitch.text()
+            starts = self.threadingStarts.currentText()
+            x_start = self.threadingXStart.text()
+            x_end = self.threadingXEnd.text()
+            infeed_angle = self.threadingCompAngle.text()
+
+            self.labelThreadType.setText(f"External, [{pitch}]mm, [{starts} starts]"
+                                         if self.isExternalThread
+                                         else f"Internal, [{pitch}]mm, [{starts} starts]")
+            self.labelMajorDiameter.setText(x_start if self.isExternalThread else x_end)
+            self.labelMinorDiameter.setText(x_end if self.isExternalThread else x_start)
+            self.labelZEnd.setText(self.threadingZEnd.text())
+            self.labelDepthDegression.setText(self.threadingDepthDegression.currentText())
+            self.labelFirstPassDoc.setText(self.threadingFirstPass.text())
+            self.labelTaperLocation.setText(self.threadingTaper.currentText())
+            self.labelSpringPasses.setText(self.threadingSpringPasses.currentText())
+            self.updateValues()
+        if page == Page.THREADING_5:
+            self.currentThreadXStart.setText(str(self.last_thread_x_start))
+            self.currentThreadZStart.setText(str(self.last_thread_z_start))
+            self.lastXEnd.setText(str(self.last_thread_x_end))
 
     def openNumPad(self, line_edit):
         setting_name = getattr(line_edit, 'settingName', None)
@@ -171,7 +248,7 @@ class QuickCycles(QWidget):
                 doc = self.radiusDoc.text()
                 location = self.radiusLocation.currentIndex()
                 return f"o<radius> call [#<_x> * 2] [#<_z>] [{corner_x}] [{corner_z}] [{radius}] [{doc}] [{location}]"
-            case Page.THREADING.index:
+            case Page.THREADING_3.index:
                 pitch = self.threadingPitch.text()
                 starts = self.threadingStarts.currentText()
                 z_end = self.threadingZEnd.text()
@@ -182,6 +259,34 @@ class QuickCycles(QWidget):
                 infeed_angle = self.threadingCompAngle.text()
                 taper = self.threadingTaper.currentIndex()
                 spring_passes = self.threadingSpringPasses.currentText()
+
+                self.last_thread_x_end = x_end
+                return (f"o<threading> call "
+                        f"[{x_start}] "
+                        f"[#<_z>] "
+                        f"[{x_end}] "
+                        f"[{z_end}] "
+                        f"[{pitch}] "
+                        f"[{starts}] "
+                        f"[{first_pass}] "
+                        f"[{depth_degression}] "
+                        f"[{infeed_angle}] "
+                        f"[{taper}] "
+                        f"[{45}] "
+                        f"[{spring_passes}]")
+            case Page.THREADING_5.index:
+                pitch = self.threadingPitch.text()
+                starts = self.threadingStarts.currentText()
+                z_end = self.threadingZEnd.text()
+                x_start = self.threadingXStart.text()
+                x_end = self.newXEnd.text()
+                first_pass = float(self.last_thread_x_end) - float(x_end)
+                depth_degression = self.threadingDepthDegression.currentText()
+                infeed_angle = self.threadingCompAngle.text()
+                taper = self.threadingTaper.currentIndex()
+                spring_passes = self.threadingNewSpringPasses.currentText()
+
+                self.last_thread_x_end = x_end
                 return (f"o<threading> call "
                         f"[{x_start}] "
                         f"[#<_z>] "
@@ -211,5 +316,38 @@ class QuickCycles(QWidget):
             case _:
                 return ""
 
-    def onBtnLoadClicked(self):
-        self.onLoadClicked.emit(self._getSubroutineToCall())
+    def onThreadingFinished(self):
+        self.switchPage(Page.ROOT)
+        self.last_thread_x_start = None
+        self.last_thread_z_start = None
+
+    def onThreadingGoDeeper(self):
+        self.switchPage(Page.THREADING_5)
+
+    def btnOnBackClicked(self):
+        match self.stackedWidget.currentIndex():
+            case Page.THREADING_2.index:
+                self.switchPage(Page.THREADING_1)
+            case Page.THREADING_3.index:
+                self.switchPage(Page.THREADING_2)
+            case _:
+                self.switchPage(Page.ROOT)
+
+    def onBtnNextClicked(self):
+        match self.stackedWidget.currentIndex():
+            case Page.THREADING_1.index:
+                self.switchPage(Page.THREADING_2)
+            case Page.THREADING_2.index:
+                self.switchPage(Page.THREADING_3)
+            case _:
+                self.onLoadClicked.emit(self._getSubroutineToCall())
+
+    def moveToPosX(self):
+        if self.last_thread_x_start is None:
+            return
+        issue_mdi(f"G0 X{self.last_thread_x_start}")
+
+    def moveToPosZ(self):
+        if self.last_thread_z_start is None:
+            return
+        issue_mdi(f"G0 Z{self.last_thread_z_start}")
